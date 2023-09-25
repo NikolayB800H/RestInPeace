802dbfe1fa2cfff080281493198915bcd9189011
Nikolay Gorkunov <nichto77@gmail.com>
2023-09-25 15:58:26 +0300

    finished

--- awesomeProject/internal/api/server.go
+++ awesomeProject/internal/api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +14,7 @@ type ForecastType struct {
 	Header  string
 	Details string
 	More    string
+	Id      string
 }
 
 func StartServer() {
@@ -22,39 +25,62 @@ func StartServer() {
 			"Прогноз температуры",
 			"Точность ±50℃",
 			"Наши термометры самые точные в мире!!!! Купи прогноз, не пожалеешь!",
+			"1",
 		}, {
-			"term.svg",
+			"gau.svg",
 			"Прогноз давления",
 			"Точность ±42кПа",
 			"Наши манометры самые точные в мире!!!! Купи прогноз, не пожалеешь!",
+			"2",
 		}, {
-			"term.svg",
+			"rain.svg",
 			"Прогноз влажности",
 			"Точность ±66%",
 			"Наши термометры самые точные в мире!!!! Купи прогноз, не пожалеешь!",
+			"3",
 		},
 	}
-	log.Println(data)
 
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
 	r.LoadHTMLGlob("templates/*")
 
-	r.GET("/index", func(c *gin.Context) {
+	/*r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"choose": true,
+			"data":   data,
+			"value":  "",
+		})
+	})*/
+
+	r.GET("/more/:id", func(c *gin.Context) {
+		id_str := c.Param("id")
+		id, err := strconv.Atoi(id_str)
+		if err != nil || id < 1 || id > 3 {
+			c.String(http.StatusNotFound, "Такого прогноза нет!")
+			return
+		}
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"choose": false,
+			"id":     data[id-1],
 		})
 	})
 
-	r.GET("/more", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
+		value := c.Query("index")
+		log.Println("index", value)
+		var forecasts [] /*len(data)*/ ForecastType
+		//j := 0
+		for i := 0; i < len(data); i++ {
+			if strings.HasPrefix(data[i].Header, value) {
+				//forecasts[j] = data[i]
+				//j++
+				forecasts = append(forecasts, data[i])
+			}
+		}
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"choose": false,
+			"choose": true,
+			"data":   forecasts,
 		})
 	})
 
