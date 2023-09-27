caab44f7c6ce664dc30fa7c2fb95654b4f45ebcd
Nikolay Gorkunov <nichto77@gmail.com>
2023-09-27 07:12:06 +0300

    finished2

--- awesomeProject/internal/api/server.go
+++ awesomeProject/internal/api/server.go
@@ -15,43 +15,40 @@ type ForecastType struct {
 	Details string
 	More    string
 	Id      string
+	Unit    string
 }
 
 func StartServer() {
 	log.Println("Server start up")
+
 	data := [3]ForecastType{
 		{
 			"term.svg",
 			"Прогноз температуры",
-			"Точность ±50℃",
+			"Точность ±50",
 			"Наши термометры самые точные в мире!!!! Купи прогноз, не пожалеешь!",
 			"1",
+			"℃",
 		}, {
 			"gau.svg",
 			"Прогноз давления",
-			"Точность ±42кПа",
+			"Точность ±42",
 			"Наши манометры самые точные в мире!!!! Купи прогноз, не пожалеешь!",
 			"2",
+			"мм рт. ст.",
 		}, {
 			"rain.svg",
 			"Прогноз влажности",
-			"Точность ±66%",
+			"Точность ±66",
 			"Наши термометры самые точные в мире!!!! Купи прогноз, не пожалеешь!",
 			"3",
+			"%",
 		},
 	}
 
 	r := gin.Default()
 
 	r.LoadHTMLGlob("templates/*")
-
-	/*r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"choose": true,
-			"data":   data,
-			"value":  "",
-		})
-	})*/
 
 	r.GET("/more/:id", func(c *gin.Context) {
 		id_str := c.Param("id")
@@ -69,18 +66,16 @@ func StartServer() {
 	r.GET("/", func(c *gin.Context) {
 		value := c.Query("index")
 		log.Println("index", value)
-		var forecasts [] /*len(data)*/ ForecastType
-		//j := 0
+		var forecasts []ForecastType
 		for i := 0; i < len(data); i++ {
-			if strings.HasPrefix(data[i].Header, value) {
-				//forecasts[j] = data[i]
-				//j++
+			if strings.Contains(strings.ToLower(data[i].Header), strings.ToLower(value)) {
 				forecasts = append(forecasts, data[i])
 			}
 		}
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"choose": true,
 			"data":   forecasts,
+			"value":  value,
 		})
 	})
 
