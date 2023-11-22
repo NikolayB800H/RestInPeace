87148a9b8f8f625e36bdbaed097e13909bac2d80
Nikolay Gorkunov <nichto77@gmail.com>
2023-11-22 08:32:12 +0300

    66.7%

--- awesomeProject/insert.sql
+++ awesomeProject/insert.sql
@@ -8,9 +8,9 @@ VALUES ('796c70e1-5f27-4433-a415-95e7272effa5' , 'moderator', 'pass_moderator', 
        ('5f58c307-a3f2-4b13-b888-c80ad08d5ed3' , 'client', 'pass_client', FALSE);
 
 INSERT INTO data_types(data_type_id, image_path, data_type_name, precision, description, unit, data_type_status)
-VALUES ('a20163ce-7be5-46ec-a50f-a313476b2bd1' , 'term.svg', 'температуры', 5.0, 'Наши термометры самые точные в мире!!!! Купи прогноз, не пожалеешь!', '℃', 'valid'),
-       ('0706419e-b024-469d-a354-9480cd79c6a5' , 'gau.svg', 'давления', 4.2, 'Наши манометры самые точные в мире!!!! Купи прогноз, не пожалеешь!', 'мм рт. ст.', 'valid'),
-       ('8f157a95-dad1-43e0-9372-93b51de06163' , 'rain.svg', 'влажности', 6.6, 'Наши гигрометры самые точные в мире!!!! Купи прогноз, не пожалеешь!', '%', 'valid');
+VALUES ('a20163ce-7be5-46ec-a50f-a313476b2bd1' , 'localhost:9000/images/a20163ce-7be5-46ec-a50f-a313476b2bd1.jpg', 'температуры', 5.0, 'Наши термометры самые точные в мире!!!! Купи прогноз, не пожалеешь!', '℃', 'valid'),
+       ('0706419e-b024-469d-a354-9480cd79c6a5' , 'localhost:9000/images/0706419e-b024-469d-a354-9480cd79c6a5.jpg', 'давления', 4.2, 'Наши манометры самые точные в мире!!!! Купи прогноз, не пожалеешь!', 'мм рт. ст.', 'valid'),
+       ('8f157a95-dad1-43e0-9372-93b51de06163' , 'localhost:9000/images/8f157a95-dad1-43e0-9372-93b51de06163.jpg', 'влажности', 6.6, 'Наши гигрометры самые точные в мире!!!! Купи прогноз, не пожалеешь!', '%', 'valid');
 
 INSERT INTO forecast_applications(application_id, application_status, application_creation_date, application_formation_date, application_completion_date, creator_id, moderator_id, input_start_date)
 VALUES ('b0247ccd-28ab-45be-9680-f24213cf7aab', 'удалён', '2023-10-25', '2023-10-25', '2023-10-25', '796c70e1-5f27-4433-a415-95e7272effa5', '5f58c307-a3f2-4b13-b888-c80ad08d5ed3', '2023-10-22');
